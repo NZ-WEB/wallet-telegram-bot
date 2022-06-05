d02bd9ae904184a18b798c4e7873d3a5436d8663
NZ-WEB <stereo00@yandex.ru>
2022-06-05 15:47:21 +0300

    add wallet

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import { Markup, Telegraf } from "telegraf";
+require("dotenv").config();
+
+import { Money, Currencies } from "ts-money";
+import axios from "axios";
+import Wallet from "./commands/wallet/wallet";
+/**
+ * TELEGRAM_BOT_TOKEN
+ * Can find in .env file
+ */
+const TELEGRAM_BOT_TOKEN: string = process.env.TELEGRAM_BOT_TOKEN!;
+const CURRENCY_ACCESS_KEY: string = process.env.CURRENCY_ACCESS_KEY!;
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+bot.use(Telegraf.log());
+
+const wallet = Wallet.getInstance();
+wallet.setInitWallet("RUB", 1000);
+wallet.addWallet("RUB", 1000);
+
+console.log(wallet);
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
